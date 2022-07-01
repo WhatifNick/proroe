7b6296e0a4da5bfc28f08429707a3a6aebd5f6a5
Nick Pasquariello <npasquariello@gmail.com>
2022-07-01 17:29:38 +1000

    Initial Push

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function () {
+    // init feather icons
+    feather.replace();
+});
+
+function copyEvent()
+{
+    navigator.clipboard.writeText("proroe5k@gmail.com");
+
+    /* Alert the copied text */
+    // alert("Copied!");
+}
