3f3052518a9030a35e6041584d70eaef7ea4bd27
Nick Pasquariello <npasquariello@gmail.com>
2022-07-04 16:36:40 +1000

    Removed copy button and changed text

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,32 @@ $(function () {
     feather.replace();
 });
 
-function copyEvent()
-{
-    navigator.clipboard.writeText("proroe5k@gmail.com");
-
-    /* Alert the copied text */
-    // alert("Copied!");
-}
+// function copyToClipboard() {
+//     let text = document.querySelector("#email").textContent;
+//     console.log(111, text)
+//     text.select();
+//     text = text.value;
+  
+//     if (window.clipboardData && window.clipboardData.setData) {
+//       // IE: prevent textarea being shown while dialog is visible
+//       return window.clipboardData.setData("Text", text);
+  
+//     } else if (document.queryCommandSupported && 
+//                document.queryCommandSupported("copy")) {
+//       var textarea = document.createElement("textarea");
+//       textarea.textContent = text;
+//       // Prevent scrolling to bottom of page in MS Edge
+//       textarea.style.position = "fixed";
+//       document.body.appendChild(textarea);
+//       textarea.select();
+//       try {
+//         // Security exception may be thrown by some browsers
+//         return document.execCommand("copy");
+//       } catch (ex) {
+//         console.warn("Copy to clipboard failed.", ex);
+//         return false;
+//       } finally {
+//         document.body.removeChild(textarea);
+//       }
+//     }
+//   }
